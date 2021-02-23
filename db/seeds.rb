0365fa01f0639165f71572bb18b6a5dee79ddf66
Tom Louwagie <tom@louwagie.com>
2021-02-23 16:58:42 +0100

    Epic spaces index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
 # Seeding the database in order to test code
 u1 = User.create(email: "dummy@gmail.com", password: "costudyo")
 
-s1 = Space.create(user: u1, title: "Epic study room", location: "Munich", description:"Super fast internet. 100% focus guaranteed.", price: 10)
-s2 = Space.create(user: u1, title: "Zen Island", location: "Berlin", description:"Quiet. Free coffee. Comfy chairs.", price: 20)
+Space.create(user: u1, title: "Epic study room", location: "Munich", description:"Super fast internet. 100% focus guaranteed.", price: 10)
+Space.create(user: u1, title: "Zen Island", location: "Berlin", description:"Quiet. Free coffee. Comfy chairs.", price: 20)
+Space.create(user: u1, title: "Studio", location: "Berlin", description:"Quiet. Free coffee. Comfy chairs.", price: 20)
+Space.create(user: u1, title: "House study", location: "Munich", description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit.", price: 20)
+Space.create(user: u1, title: "Studnet paradise", location: "Berlin", description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit.", price: 20)
+Space.create(user: u1, title: "No study. No gain.", location: "Munich", description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit.", price: 20)
+Space.create(user: u1, title: "Focus zone", location: "Berlin", description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit.", price: 20)
