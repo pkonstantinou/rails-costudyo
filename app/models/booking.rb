bfabb90b981fb957e921e9be0dff89b88362bb19
Paschalis Konstantinou <pasc.konstantinou@gmail.com>
2021-02-25 15:52:49 +0100

    Initial commit

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
-  belongs_to :user
-  belongs_to :space
+  belongs_to :user, dependent: :destroy
+  belongs_to :space, dependent: :destroy
 
   validates :starting_date, presence: true
   validates :ending_date, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :spaces, only: %i[index show] do
+  get '/dashboard', to: 'pages#dashboard'
+  resources :spaces do
     resources :bookings, only: %i[create]
   end
+  delete '/bookings/:id', to: 'bookings#destroy', as: 'booking'
   get '/bookings/:id', to: 'bookings#confirm', as: 'booking_confirmation'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
