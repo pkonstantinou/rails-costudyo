7dcf6eef6e2e46d512e075854820ecae20f7ae66
Tom Louwagie <tom@louwagie.com>
2021-02-25 17:29:14 +0100

    first version of bookings overview

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,6 +1,6 @@
 class Space < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
   has_many_attached :photos
 
